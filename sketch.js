686eeabf0123acefad82f736aedeac4cb438ed5e
luizribeirosilva08-gif <luiz.ribeiro.silva08@escola.pr.gov.br>
2026-09-04 11:14:35 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+//Projeto 5 - Controlando o mouse
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(mouseX,mouseY,20)
+  if(mouseX > width){
+    text("Mouse fora da tela",150, 200);
+  }
+  if(mouseY > height){
+    text("Mouse forra da tela", 150, 200);
+  }
+
+  if(mouseX < 0){
+    text("Mouse fora da tela,", 150, 200)
+  }
+}
